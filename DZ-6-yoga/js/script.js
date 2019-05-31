5cb49b9ce60339e52cc396087a89c87dff2a7323
Anna Khodunova <khodunovanna@gmail.com>
2019-05-31 13:49:53 -0700

    DZ-6-yoga самостоятельное задание
    добавила в случае ошибки вывод таймера 00:00:00, и вместо 4:5:50 выводилось 04:05:50

--- DZ-6-yoga/js/script.js
+++ DZ-6-yoga/js/script.js
@@ -34,4 +34,55 @@ window.addEventListener('DOMContentLoaded', function() {
       }
   });
 
+  //Timer
+
+  let deadline = '2019-06-01';
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()), //parse-превращает любую дату в милисекунды
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/1000/60/60));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+          let t = getTimeRemaining(endtime);
+          hours.textContent = t.hours;
+          minutes.textContent = t.minutes;
+          seconds.textContent = t.seconds;
+
+          if(seconds.textContent.length<2){
+            seconds.textContent = '0' + t.seconds;
+          }
+          if(minutes.textContent.length<2){
+            minutes.textContent = '0' + t.minutes;
+          }
+          if(hours.textContent.length<2){
+            hours.textContent = '0' + t.hours;
+          }
+
+          if (t.total <= 0) {
+            clearInterval(timeInterval);
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
+          }
+        }
+  }
+  setClock('timer', deadline);
+
 });
